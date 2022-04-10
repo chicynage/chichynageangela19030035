05c2e33e3e3e903c6a5ab8be3095fd853e681fe1
chicynage <chicynag24@gmail.com>
2022-04-10 22:46:43 +0700

    Tugas

---
+++ app/src/main/java/com/example/tugas2pratikum19030035/layoutskor.java
@@ -0,0 +1,14 @@
+package com.example.tugas2pratikum19030035;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class layoutskor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layoutskor);
+    }
+}
